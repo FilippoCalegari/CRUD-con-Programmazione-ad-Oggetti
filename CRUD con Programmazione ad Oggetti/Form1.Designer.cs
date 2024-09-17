cf92d57b6816c06e1145a24a837cc739abe5b6e5
Calegari.20105 <Calegari.20105@LABI1-6PC16>
2024-09-17 09:06:57 +0200

    Aggiungere i file di progetto.

---
+++ CRUD con Programmazione ad Oggetti/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace CRUD_con_Programmazione_ad_Oggetti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_create = new System.Windows.Forms.Button();
+            this.lbl_artistName = new System.Windows.Forms.Label();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.tb_artistName = new System.Windows.Forms.TextBox();
+            this.tb_albumName = new System.Windows.Forms.TextBox();
+            this.lbl_albumName = new System.Windows.Forms.Label();
+            this.list_records = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_serialize = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_create
+            // 
+            this.btn_create.Location = new System.Drawing.Point(12, 95);
+            this.btn_create.Name = "btn_create";
+            this.btn_create.Size = new System.Drawing.Size(140, 140);
+            this.btn_create.TabIndex = 0;
+            this.btn_create.Text = "AGGIUNGI";
+            this.btn_create.UseVisualStyleBackColor = true;
+            this.btn_create.Click += new System.EventHandler(this.btn_create_Click);
+            // 
+            // lbl_artistName
+            // 
+            this.lbl_artistName.AutoSize = true;
+            this.lbl_artistName.Location = new System.Drawing.Point(12, 9);
+            this.lbl_artistName.Name = "lbl_artistName";
+            this.lbl_artistName.Size = new System.Drawing.Size(70, 13);
+            this.lbl_artistName.TabIndex = 1;
+            this.lbl_artistName.Text = "Nome Artista:";
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(158, 95);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(140, 140);
+            this.btn_update.TabIndex = 2;
+            this.btn_update.Text = "MODIFICA";
+            this.btn_update.UseVisualStyleBackColor = true;
+            // 
+            // btn_read
+            // 
+            this.btn_read.Location = new System.Drawing.Point(12, 241);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(140, 140);
+            this.btn_read.TabIndex = 3;
+            this.btn_read.Text = "LEGGI";
+            this.btn_read.UseVisualStyleBackColor = true;
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(158, 241);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(140, 140);
+            this.btn_delete.TabIndex = 4;
+            this.btn_delete.Text = "ELIMINA";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            // 
+            // tb_artistName
+            // 
+            this.tb_artistName.Location = new System.Drawing.Point(12, 25);
+            this.tb_artistName.Name = "tb_artistName";
+            this.tb_artistName.Size = new System.Drawing.Size(286, 20);
+            this.tb_artistName.TabIndex = 5;
+            // 
+            // tb_albumName
+            // 
+            this.tb_albumName.Location = new System.Drawing.Point(12, 69);
+            this.tb_albumName.Name = "tb_albumName";
+            this.tb_albumName.Size = new System.Drawing.Size(286, 20);
+            this.tb_albumName.TabIndex = 7;
+            // 
+            // lbl_albumName
+            // 
+            this.lbl_albumName.AutoSize = true;
+            this.lbl_albumName.Location = new System.Drawing.Point(12, 53);
+            this.lbl_albumName.Name = "lbl_albumName";
+            this.lbl_albumName.Size = new System.Drawing.Size(70, 13);
+            this.lbl_albumName.TabIndex = 6;
+            this.lbl_albumName.Text = "Nome Album:";
+            // 
+            // list_records
+            // 
+            this.list_records.FormattingEnabled = true;
+            this.list_records.Location = new System.Drawing.Point(325, 26);
+            this.list_records.Name = "list_records";
+            this.list_records.Size = new System.Drawing.Size(371, 355);
+            this.list_records.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(322, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Lista Artisti e Album:";
+            // 
+            // btn_serialize
+            // 
+            this.btn_serialize.Location = new System.Drawing.Point(719, 25);
+            this.btn_serialize.Name = "btn_serialize";
+            this.btn_serialize.Size = new System.Drawing.Size(140, 170);
+            this.btn_serialize.TabIndex = 10;
+            this.btn_serialize.Text = "SALVA";
+            this.btn_serialize.UseVisualStyleBackColor = true;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(719, 211);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(140, 170);
+            this.btn_exit.TabIndex = 11;
+            this.btn_exit.Text = "ESCI";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 571);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_serialize);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.list_records);
+            this.Controls.Add(this.tb_albumName);
+            this.Controls.Add(this.lbl_albumName);
+            this.Controls.Add(this.tb_artistName);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_read);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.lbl_artistName);
+            this.Controls.Add(this.btn_create);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_create;
+        private System.Windows.Forms.Label lbl_artistName;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.TextBox tb_artistName;
+        private System.Windows.Forms.TextBox tb_albumName;
+        private System.Windows.Forms.Label lbl_albumName;
+        private System.Windows.Forms.ListBox list_records;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_serialize;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
+
---
+++ CRUD con Programmazione ad Oggetti/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_con_Programmazione_ad_Oggetti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_create_Click(object sender, EventArgs e)
+        {
+            string artistName = tb_artistName.Text.ToString();
+            string albumName = tb_albumName.Text.ToString();
+            Record record = new Record(artistName, albumName);
+
+            list_records.Items.Add(record.Artist, record.Album);
+        }
+    }
+    class Record
+    {
+        private string _artist;
+        public string Artist
+        {
+            get => _artist;
+            set => _artist = value;
+        }
+        private string _album;
+        public string Album
+        {
+            get => _album;
+            set => _album = value;
+        }
+        public Record(string artist, string album)
+        {
+            Artist = artist;
+            Album = album;
+        }
+    }
+}
